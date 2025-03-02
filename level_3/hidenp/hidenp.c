a61aa446c1cc15e5d487510745224cbac4f457bd
Danielnf777 <daninf77@proton.me>
2025-03-02 20:45:57 +0100

    update hidenp

--- level_3/hidenp/hidenp.c
+++ level_3/hidenp/hidenp.c
@@ -1,30 +1,37 @@
-#include <unistd.h>
+# include <unistd.h>
 
 int	main(int ac, char **av)
 {
 	int	i;
 	int	j;
-	int	temp;
 
 	i = 0;
 	j = 0;
-	temp = 0;
-	
-	if (ac == 3)
+	if(ac == 3)
 	{
-		while (av[1][temp])
-			temp++;
-		while (av[2][j])
-		{
-			if (av[1][i] == av[2][j])
-				i++;
-			j++;
-		}
-		if (temp == i)
+		if (!av[1][j])
 			write(1, "1", 1);
-		else
-			write(1, "0", 1);
+		while (av[1][i])
+		{
+			while (av[2][j])
+			{
+				if (av[1][i] == av[2][j])
+				{
+					j++;
+					break;
+				}
+				j++;
+			}
+			i++;
+			if (av[1][i] == '\0')
+				write(1, "1", 1);
+			else if (av[2][j] == '\0')
+			{
+				write(1, "0", 1);
+				break;
+			}
+		}
 	}
 	write(1, "\n", 1);
-	return (0);
 }
+				
