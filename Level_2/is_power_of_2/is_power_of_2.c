e84ac4e948629b34689184f4e94a1b5643fd09aa
Danielnf777 <daninf77@proton.me>
2025-03-02 18:36:51 +0100

    update

--- Level_2/is_power_of_2/is_power_of_2.c
+++ Level_2/is_power_of_2/is_power_of_2.c
@@ -1,4 +1,20 @@
 int         is_power_of_2(unsigned int n)
 {
-	    return (n > 0) && ((n & (n - 1)) == 0);
+	if (n == 0)
+		return (0);
+	if ((n & (n - 1)) == 0)
+		return (1);
+	else
+		return (0);
 }
+/*
+#include <stdio.h>
+int main()
+{
+	int	i;
+
+	i = is_power_of_2(1024);
+	printf("%d\n", i);
+	return (0);
+}
+*/
