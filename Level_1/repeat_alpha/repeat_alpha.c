3a19920183477bb000d68dbc199762edab37dd69
Danielnf777 <daninf77@proton.me>
2025-03-09 20:10:45 +0100

    one more

--- Level_1/repeat_alpha/repeat_alpha.c
+++ Level_1/repeat_alpha/repeat_alpha.c
@@ -10,12 +10,11 @@ int	main(int ac, char **av)
 	{
 		while (av[1][i])
 		{
+			count = 1;
 			if (av[1][i] >= 'a' && av[1][i] <= 'z')
 				count = av[1][i] - 'a' + 1;
-			else if (av[1][i] >= 'A' && av[1][i] <= 'Z')
+			if (av[1][i] >= 'A' && av[1][i] <= 'Z')
 				count = av[1][i] - 'A' + 1;
-			else
-				count = 1;
 			while (count--)
 				write(1, &av[1][i], 1);
 			i++;
@@ -24,5 +23,3 @@ int	main(int ac, char **av)
 	write(1, "\n", 1);
 	return (0);
 }
-
-
